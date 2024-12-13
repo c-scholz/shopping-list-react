bff047631f1100fb4cc34e2879b317907cedc053
c-scholz <scholzcedric@web.de>
2024-12-13 13:58:30 +0100

    prettied up and implemented removal of list

--- src/modules/Layout/AppNav.tsx
+++ src/modules/Layout/AppNav.tsx
@@ -1,10 +1,11 @@
 import { NavLink } from 'react-router-dom'
+import Logo from '../../assets/logo.png'
 import './AppNav.css'
 
 export const AppNav = () => {
   return (
     <nav className="app-nav">
-      <NavLink to="/">Dashboard</NavLink>
+      <NavLink className="app-nav__home" to="/" title="Dashboard"><img className="app-nav__logo" src={Logo} /><span className="app-nav__name">ShopXip</span></NavLink>
     </nav>
   )
 }
--- src/modules/List/ShoppingListCard.tsx
+++ src/modules/List/ShoppingListCard.tsx
@@ -2,23 +2,44 @@ import "./ShoppingListCard.css"
 import { Link } from 'react-router-dom'
 import { ShoppingList } from './types'
 
-export const ShoppingListCard = ({ id, name, items }: ShoppingList) => {
+type ShoppingListCard = {
+  list: ShoppingList,
+  removeList: (list: ShoppingList) => void
+}
+
+export const ShoppingListCard = (props: ShoppingListCard) => {
+  const { list, removeList } = props
+
   return (
     <div className="card">
-      <Link to={`/list/${id}`}><h2>{name}</h2></Link>
-      {items.length < 1 && (
+      <div className="card__header">
+        <Link
+          to={`/list/${list.id}`}
+          title={`Go to list: ${list.name}`}
+        >
+          <h2>{list.name}</h2>
+        </Link>
+        <button
+          className="card__remove-list"
+          onClick={() => removeList(list)}
+          title={`Remove list: ${list.name}`}
+        >
+          <i className="pi pi-times item__remove-icon " />
+        </button>
+      </div>
+      {list.items.length < 1 && (
         <div className="card__no-entries">
           No entries yet
         </div>
       )}
       <ul className="card__entries">
-        {items.slice(0, 3).map((item, idx) => (
+        {list.items.slice(0, 3).map((item, idx) => (
           <li key={idx}>
             {item.name} {item.amount}x
           </li>
         ))}
       </ul>
-      {items.length > 4 && (<div className="card__more-entries">...</div>)}
+      {list.items.length > 4 && (<div className="card__more-entries">...</div>)}
     </div>
   )
 }
